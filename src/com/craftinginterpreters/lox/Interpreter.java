f55497dc6eff7c014d6c14cba9becdf9323b2bc9
Stefanos Valoumas <stefanos.valoumas@orfium.com>
2021-07-11 18:36:46 +0300

    style: change method position and minor changes

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -11,6 +11,10 @@ class Interpreter implements Expr.Visitor<Object> {
         }
     }
 
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -19,10 +23,6 @@ class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
-    }
-
-    private Object evaluate(Expr expr) {
-        return expr.accept(this);
     }
 
     @Override
